b954b4ade5f6b975352c36ecde70ee8bc96e25b8
Paulina Sak <paulinasak0@gmail.com>
2016-05-04 14:20:40 +0200

    Zrobione

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,11 +1,28 @@
-describe('app', function () {
-    'use strict';
+(function () {
+            'use strict';
 
-    var answer = window.app;
+            window.app = {
 
-    describe('calculateArea', function () {
-        it('should should...', function () {
+                        describe('calculateArea', function () {
+                                    it('should return area of figure & message', function () {
+                                                expect(answer.calculateArea(6,1,3,'Ok', 'Fail')).toEqual({area: 3, message: 'Ok'});
+                                                expect(answer.calculateArea(2,6,6,'Ok', 'Fail')).toEqual({area: -6, message: 'Fail'});
+                                                expect(answer.calculateArea(6,6,6,'Ok', 'Fail')).toEqual({area: -30, message: 'Fail'});
+                                                expect(answer.calculateArea(13,6,2,'Ok', 'Fail')).toEqual({area: 1, message: 'Ok'});
+                                                expect(answer.calculateArea(5,1,1,'Ok', 'Fail')).toEqual({area: 4, message: 'Ok'});
 
-        });
-    });
+                                    });
+                                    it('should return area of figure & message', function () {
+                                                expect(answer.calculateArea(6,"x",3,'Ok', 'Fail')).toEqual(false);
+                                                expect(answer.calculateArea(6,1,'','Ok', 'Fail')).toEqual(false);
+                                                expect(answer.calculateArea("",1,'','Ok', 'Fail')).toEqual(false);
+                                    });
+
+                                    it('should return area 0 & message "Big null"', function () {
+                                                expect(answer.calculateArea(0,0,0,'Ok', 'Fail')).toEqual({area: 0, message:'Big null'});
+
+                                    });
+
+                        });
 });
+
